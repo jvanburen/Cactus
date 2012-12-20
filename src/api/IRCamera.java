b4490a1fcb2df42625b014b512c17205d24e17a7
Jacob Van Buren <JacobAndrewVB@gmail.com>
2012-12-19 21:38:19 -0500

    Created empty IRCamera class

---
+++ src/api/IRCamera.java
@@ -0,0 +1,11 @@
+
+package api;
+
+/**
+ * A class that abstracts an infrared sensor/camera taken from a Wii(r) remote
+ * attached to an I2C port on Cactus.
+ * @author Jacob
+ */
+public class IRCamera {
+    
+}
