6c614427cc959869d0d77d974e1b71e9e3a3c599
Jacob <Jacob@172.16.241.22>
2012-12-28 17:59:03 -0500

    Removed unused fields

--- src/api/Blob.java
+++ src/api/Blob.java
@@ -2,7 +2,7 @@
 package api;
 
 /**
- * A class to represent a blob as detected by the IR camera in full mode.
+ * A class to represent a blob as detected by the IR camera in Extended mode.
  * @author Jacob Van Buren
  * @version 0.0.1
  * @since 2.0.2
@@ -16,21 +16,21 @@ public class Blob {
 
     /** The size of the blob. */
     public byte size;
-
-    /** Part of the bounding box of the blob. */
-    public byte xMin;
-
-    /** Part of the bounding box of the blob. */
-    public byte xMax;
-
-    /** Part of the bounding box of the blob. */
-    public byte yMin;
-
-    /** Part of the bounding box of the blob. */
-    public byte yMax;
-
-    /** The intensity of the blob. */
-    public char intensity;
+//// These fields only used in Full mode
+//    /** Part of the bounding box of the blob. */
+//    public byte xMin;
+//
+//    /** Part of the bounding box of the blob. */
+//    public byte xMax;
+//
+//    /** Part of the bounding box of the blob. */
+//    public byte yMin;
+//
+//    /** Part of the bounding box of the blob. */
+//    public byte yMax;
+//
+//    /** The intensity of the blob. */
+//    public char intensity;
 
 
 }
